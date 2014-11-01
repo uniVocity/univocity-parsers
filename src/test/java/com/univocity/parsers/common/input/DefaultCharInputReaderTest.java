c7b8e7b4a1901566e2d9e5224f9b7476eb678bb3
Jeronimo Backes <jbax@univocitysoftware.com>
2014-11-02 04:08:00 +1030

    Revert "fixed broken unit test after changes in EOF handling"
    This reverts commit b934bbb039cf34cb93c1d99209533bddf1a792bc.

--- src/test/java/com/univocity/parsers/common/input/DefaultCharInputReaderTest.java
+++ src/test/java/com/univocity/parsers/common/input/DefaultCharInputReaderTest.java
@@ -1,12 +1,12 @@
 /*******************************************************************************
  * Copyright 2014 uniVocity Software Pty Ltd
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -23,46 +23,37 @@ import org.testng.annotations.*;
 
 public class DefaultCharInputReaderTest {
 
-	private void assertEOF(DefaultCharInputReader reader) {
-		try {
-			reader.nextChar();
-			fail("Expected EOFException");
-		} catch (EOFException ex) {
-			//pass
-		}
-	}
-
 	@Test
 	public void testInputReading() {
 		DefaultCharInputReader reader = new DefaultCharInputReader("\n\r".toCharArray(), '\n', 2);
 
 		reader.start(new StringReader("a"));
 		assertEquals('a', reader.nextChar());
-		assertEOF(reader);
+		assertEquals('\0', reader.nextChar());
 
 		reader.start(new StringReader("ab"));
 		assertEquals('a', reader.nextChar());
 		assertEquals('b', reader.nextChar());
-		assertEOF(reader);
+		assertEquals('\0', reader.nextChar());
 
 		reader.start(new StringReader("a\n\r"));
 		assertEquals('a', reader.nextChar());
 		assertEquals('\n', reader.nextChar());
-		assertEOF(reader);
+		assertEquals('\0', reader.nextChar());
 
 		reader.start(new StringReader("a\r\n"));
 		assertEquals('a', reader.nextChar());
 		assertEquals('\r', reader.nextChar());
 		assertEquals('\n', reader.nextChar());
-		assertEOF(reader);
+		assertEquals('\0', reader.nextChar());
 
 		reader.start(new StringReader("\n\ra"));
 		assertEquals('\n', reader.nextChar());
 		assertEquals('a', reader.nextChar());
-		assertEOF(reader);
+		assertEquals('\0', reader.nextChar());
 
 		reader.start(new StringReader("\n\r"));
 		assertEquals('\n', reader.nextChar());
-		assertEOF(reader);
+		assertEquals('\0', reader.nextChar());
 	}
 }
