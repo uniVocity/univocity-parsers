18007423fe1e61fdeae7607ef3001fad317d7540
Jeronimo Backes <jbax@univocity.com>
2015-12-06 02:46:55 +0700

    bugfixes

--- src/main/java/com/univocity/parsers/common/processor/RowProcessorSwitch.java
+++ src/main/java/com/univocity/parsers/common/processor/RowProcessorSwitch.java
@@ -1,18 +1,9 @@
-/*******************************************************************************
- * Copyright 2015 uniVocity Software Pty Ltd
+/*
+ * Copyright (c) 2015 uniVocity Software Pty Ltd. All rights reserved.
+ * This file is subject to the terms and conditions defined in file
+ * 'LICENSE.txt', which is part of this source code package.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- ******************************************************************************/
+ */
 package com.univocity.parsers.common.processor;
 
 import com.univocity.parsers.common.*;
@@ -42,7 +33,7 @@ public abstract class RowProcessorSwitch implements RowProcessor, ColumnOrderDep
 	}
 
 	@Override
-	public final void rowProcessed(String[] row, ParsingContext context) {
+	public final void rowProcessed(String[] row, final ParsingContext context) {
 		RowProcessor processor = switchRowProcessor(row, context);
 		if (processor == null) {
 			processor = NoopRowProcessor.instance;
@@ -57,7 +48,7 @@ public abstract class RowProcessorSwitch implements RowProcessor, ColumnOrderDep
 
 					@Override
 					public String[] headers() {
-						return fieldNames;
+						return fieldNames == null || fieldNames.length == 0 ? context.headers() : fieldNames;
 					}
 				};
 
