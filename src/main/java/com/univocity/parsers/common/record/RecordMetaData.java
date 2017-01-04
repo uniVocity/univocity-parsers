4c2821138ff40d0b9326e9d68763e0b6f25ea1a8
Jeronimo Backes <jbax@univocity.com>
2017-01-04 17:00:09 +1030

    Added missing javadoc to RecordMetaData#selectedHeaders

--- src/main/java/com/univocity/parsers/common/record/RecordMetaData.java
+++ src/main/java/com/univocity/parsers/common/record/RecordMetaData.java
@@ -201,6 +201,12 @@ public interface RecordMetaData {
 	 */
 	String[] headers();
 
+	/**
+	 * Returns the sequence of headers that have been selected. If no selection has been made, all available headers
+	 * will be returned, producing the same output as a call to method {@link #headers()}.
+	 *
+	 * @return the sequence of selected headers, or all headers if no selection has been made.
+	 */
 	String[] selectedHeaders();
 
 	/**
